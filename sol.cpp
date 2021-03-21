07d31e05dd0b3e1f864cb9cfd276f1a733af6309
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-03-21 21:38:50 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		int a, b;
+		cin >> a >> b;
+		cout << a * b << '\n';
+	}
+	return 0;
+}
